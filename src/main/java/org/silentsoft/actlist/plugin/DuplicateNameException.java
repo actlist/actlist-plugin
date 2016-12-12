4bd807a082c97459f68b22bcda45d3f1b846c551
silentsoft <hs830.lee@gmail.com>
2016-12-12 17:01:15 +0000

    Initial import.

---
+++ src/main/java/org/silentsoft/actlist/plugin/DuplicateNameException.java
@@ -0,0 +1,32 @@
+package org.silentsoft.actlist.plugin;
+
+import org.slf4j.helpers.MessageFormatter;
+
+public class DuplicateNameException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6678093238434689242L;
+
+	public DuplicateNameException(String message) {
+		super(message);
+	}
+	
+	public DuplicateNameException(Throwable cause) {
+		super(cause);
+	}
+	
+	public DuplicateNameException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DuplicateNameException(String message, Object[] parameters) {
+		super(MessageFormatter.arrayFormat(message, parameters).getMessage());
+	}
+	
+	public DuplicateNameException(String message, Object[] parameters, Throwable cause) {
+		super(MessageFormatter.arrayFormat(message, parameters).getMessage(), cause);
+	}
+	
+}

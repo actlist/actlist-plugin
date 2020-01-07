15bdfdf3a7157cfbb6a90374cf05fcf266165eb8
silentsoft <hyesung.lee@silentsoft.org>
2020-01-08 00:05:12 +0900

    add referencedMethodNames to AnalysisResult

--- src/main/java/org/silentsoft/actlist/plugin/AnalysisResult.java
+++ src/main/java/org/silentsoft/actlist/plugin/AnalysisResult.java
@@ -1,8 +1,12 @@
 package org.silentsoft.actlist.plugin;
 
+import java.util.List;
+
 public class AnalysisResult {
 	
 	private String minimumCompatibleVersion;
+	
+	private List<String> referencedMethodNames;
 
 	public String getMinimumCompatibleVersion() {
 		return minimumCompatibleVersion;
@@ -11,5 +15,13 @@ public class AnalysisResult {
 	public void setMinimumCompatibleVersion(String minimumCompatibleVersion) {
 		this.minimumCompatibleVersion = minimumCompatibleVersion;
 	}
-	
+
+	public List<String> getReferencedMethodNames() {
+		return referencedMethodNames;
+	}
+
+	public void setReferencedMethodNames(List<String> referencedMethodNames) {
+		this.referencedMethodNames = referencedMethodNames;
+	}
+
 }
